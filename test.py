a487f35b725b6bfc7c9cf6198a9f32182edb62cd
Longo <jlongo@lgsdirect.com>
2023-05-02 09:18:22 -0700

    show decimated output

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 fs = 36000
 duration = 0.2
-decimation = 16
+decimation = 18
 
 t = np.linspace(0, duration, round(fs * duration), endpoint=False)
 t_filtered = np.linspace(0, duration, round(fs / decimation * duration), endpoint=False)
@@ -16,11 +16,13 @@ noise_amplitude = 0.3
 noise = np.random.uniform(-noise_amplitude, noise_amplitude, len(t))
 waveform += noise
 
-filter = CICDecimatorFilter(R=decimation, N=3)
+filter = CICDecimatorFilter(R=decimation, N=6)
 filtered = filter.update(waveform)
+decimated = [x for (i, x) in enumerate(waveform) if i % decimation == 0]
 
 plt.figure()
-plt.plot(t, waveform, label="input waveform")
-plt.plot(t_filtered, filtered, label="filtered waveform")
+plt.plot(t, waveform, label="input waveform", color="blue")
+plt.plot(t_filtered, decimated, label="decimated waveform", color="green")
+plt.plot(t_filtered, filtered, label="filtered waveform", color="orange")
 plt.legend()
 plt.show()
